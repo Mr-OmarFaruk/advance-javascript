352073d4fb09ef36f0cfbf4664456296ed767bd9
Mr-OmarFaruk <fcifaruk@gmail.com>
2021-02-01 23:46:01 +0600

    truthy and  falsy

---
+++ trueFalse.js
@@ -0,0 +1,18 @@
+const age=true;
+if(age){
+    console.log("condition is true");
+    // "0"
+    // " "
+    // []
+    // {}
+    // true
+}
+else{
+    console.log("condition is false");
+//0
+//""
+//undefined
+//null
+//NaN
+//false
+}
